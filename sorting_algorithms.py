4d3f489a31eb6600b693a9ba7037cc0b78b580da
Dalekvim <dalekvim@gmail.com>
2020-08-22 11:04:55 +0100

    hid .idea file

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,10 +1,24 @@
 class ExtendedList(list):
 
+    def swap(self, i_1, i_2):
+        if i_1 >= len(self) or i_1 < 0 or i_2 >= len(self) or i_2 < 0:
+            raise IndexError("List index out of range.")
+
+        # This function alters the list by swapping the elements at the given indexes.
+        self[i_2], self[i_1] = self[i_1], self[i_2]
+        return self
+
+    def bubble_sort(self):
+        _sorted = False
+        while not _sorted:
+            _sorted = True
+            for i in range(len(self)-1):
+                i_1 = i
+                i_2 = i + 1
+                if self[i_1] > self[i_2]:
+                    self.swap(i_1, i_2)
+                    _sorted = False
+        return self
+
     def merge_sort(self):
         pass
-
-    def swap(self, index=0):
-        if index < len(self):
-            self[index], self[index + 1] = self[index + 1], self[index]
-        else:
-            raise IndexError()
--- tests.py
+++ tests.py
@@ -7,12 +7,18 @@ class MergeSortTest(unittest.TestCase):
     """
     This tests the merge_sort method of the ExtendedList.
     """
-    def test_merge_sort(self):
+    def test_swap(self):
         """This sorts the list without permanently altering it."""
         extended_list = ExtendedList()
         extended_list += [1, 2]
 
-        self.assertEqual(extended_list.swap(), [2, 1])
+        self.assertEqual(extended_list.swap(0, 1), [2, 1])
+
+        with self.assertRaises(IndexError):
+            extended_list.swap(0, 2)
+
+        with self.assertRaises(IndexError):
+            extended_list.swap(-1, 1)
 
 
 if __name__ == '__main__':
